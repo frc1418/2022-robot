209e45ebd6f8858110bcf5debde683618154e20d
BrandonWerbel <56608573+BrandonWerbel@users.noreply.github.com>
2022-02-04 15:18:58 -0500

    Add motors to storage subsystem

--- src/main/java/frc/robot/subsystems/StorageSubsystem.java
+++ src/main/java/frc/robot/subsystems/StorageSubsystem.java
@@ -1,15 +1,28 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StorageSubsystem extends SubsystemBase {
     
-    public StorageSubsystem() {
+    public final CANSparkMax storageMotorRight;
+    public final CANSparkMax storageMotorLeft;
+    public final MotorControllerGroup storageMotorGroup;
 
+    public StorageSubsystem(CANSparkMax storageMotorRight, CANSparkMax storageMotorLeft) {
+        this.storageMotorRight = storageMotorRight;
+        this.storageMotorLeft = storageMotorLeft;
+        storageMotorGroup = new MotorControllerGroup(storageMotorLeft, storageMotorRight);
     }
 
     @Override
     public void periodic(){
-        
+
+    }
+
+    public void run(double speed){
+        storageMotorGroup.set(speed);
     }
 }
