529bba06752d118d2793d5ee484c586c3d303743
Nate Berol <56608765+Eneight@users.noreply.github.com>
2022-02-05 11:51:15 -0500

    Create IntakeSubsystem

--- src/main/java/frc/robot/subsystems/IntakeSubystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubystem.java
@@ -1,16 +1,45 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
+import com.revrobotics.CANSparkMax.IdleMode;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubystem extends SubsystemBase {
+    private final CANSparkMax intakeMotor;
+    private final DoubleSolenoid leftSolenoid;
+    private final DoubleSolenoid rightSolenoid;
 
-    public IntakeSubystem() {
+    public IntakeSubystem(
+            CANSparkMax intakeMotor,
+            DoubleSolenoid leftSolenoid,
+            DoubleSolenoid rightSolenoid) {
 
+        this.intakeMotor = intakeMotor;
+        this.leftSolenoid = leftSolenoid;
+        this.rightSolenoid = rightSolenoid;
+
+        intakeMotor.setIdleMode(IdleMode.kCoast);
+    }
+
+    public void extend() {
+        leftSolenoid.set(DoubleSolenoid.Value.kReverse);
+        rightSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void retract() {
+        leftSolenoid.set(DoubleSolenoid.Value.kForward);
+        rightSolenoid.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void spin(double intakeVolts) {
+        intakeMotor.setVoltage(intakeVolts);
     }
 
     @Override
     public void periodic() {
-
+        
     }
-    
+ 
 }
