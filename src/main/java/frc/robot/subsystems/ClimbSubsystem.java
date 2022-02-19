fec7e6c435b5c699bbeacbf8987c09b888c44165
BrandonWerbel <56608573+BrandonWerbel@users.noreply.github.com>
2022-02-19 14:43:20 -0500

    Add climber position to networktables

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -4,11 +4,18 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
 
     private final TalonFX climberMotor;
+
+    private final NetworkTableInstance ntInstance = NetworkTableInstance.getDefault();
+    private final NetworkTable table = ntInstance.getTable("/components/climber");
+    private final NetworkTableEntry pos = table.getEntry("climber_pos");
 
     public ClimbSubsystem(TalonFX climberMotor) {
         this.climberMotor = climberMotor;
@@ -17,7 +24,7 @@ public class ClimbSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-
+        pos.setDouble(climberMotor.getSensorCollection().getIntegratedSensorPosition());
     }
 
     public void setWinchMotor(double speed) {
